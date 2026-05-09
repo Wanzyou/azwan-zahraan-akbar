5244e45bf95a62ca020966eaf7bdf77e6e753f54
Wanzyou <azwanakbar864@gmail.com>
2026-05-09 11:02:32 +0700

    Update portfolio website

--- script.js
+++ script.js
@@ -3,9 +3,32 @@ function showSection(sectionId){
     const sections = document.querySelectorAll('.page-section');
 
     sections.forEach(section => {
+
         section.classList.remove('active');
+
+        section.style.opacity = "0";
+        section.style.transform = "translateY(30px)";
+
     });
 
-    document.getElementById(sectionId).classList.add('active');
+    setTimeout(() => {
+
+        const activeSection = document.getElementById(sectionId);
+
+        activeSection.classList.add('active');
+
+        setTimeout(() => {
+
+            activeSection.style.opacity = "1";
+            activeSection.style.transform = "translateY(0)";
+
+        }, 50);
+
+    }, 250);
+
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    });
 
 }
